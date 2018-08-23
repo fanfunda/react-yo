f865fca4d937e98546b096f79e9812debce611d7
fan <912520775@qq.com>
2018-08-23 15:53:49 +0800

    鍩烘湰鑸炲彴

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -26,23 +26,27 @@ function getDefaultModules() {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader'
       },
       {
         test: /\.sass/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader?outputStyle=expanded&indentedSyntax'
       },
       {
         test: /\.scss/,
-        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader?outputStyle=expanded'
       },
       {
         test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
       },
       {
         test: /\.styl/,
-        loader: 'style-loader!css-loader!stylus-loader'
+        loader: 'style-loader!css-loader!autoprefixer-loader!stylus-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       },
       {
         test: /\.(png|jpg|gif|woff|woff2)$/,
@@ -60,5 +64,5 @@ module.exports = {
   srcPath: srcPath,
   publicPath: '/assets/',
   port: dfltPort,
-  getDefaultModules: getDefaultModules
+  getDefaultModules: getDefaultModules,
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,38 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//获取图片相关数据
+let imageDatas = require('../data/imageDatas.json');
+
+//利用自执行函数,讲图片信息转成图片URL路径信息
+imageDatas = (function getImageURL(imageDatasArr) {
+    // 遍历imageDatasArr数组数据
+    for (var i = 0; i < imageDatasArr.length; i++) {
+        // 获取数组中单个图片数据的信息
+        var singeImageData = imageDatasArr[i];
+        // 给单个图片数据添加图片路径信息
+        singeImageData.imageURL = require('../images/'
+            + singeImageData.fileName);
+        // 将单个图片数据重新赋值给imageDatasArr[i]
+        imageDatasArr[i] = singeImageData;
+    }
+
+    // 返回imageDatasArr
+    return imageDatasArr;
+
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <section className="stage">
+          <section className="img-sec">
+
+          </section>
+          <nav className="controller-nav">
+
+          </nav>
+        </section>
     );
   }
 }
