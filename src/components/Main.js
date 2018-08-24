6cad4dee2a6fd641f07f9de22d55bf6d27697c05
fan <912520775@qq.com>
2018-08-24 16:06:25 +0800

    鍥剧墖鏋勫缓涓€

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,36 +3,55 @@ require('styles/App.css');
 
 import React from 'react';
 
-//获取图片相关数据
-let imageDatas = require('../data/imageDatas.json');
+// 获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
 
-//利用自执行函数,讲图片信息转成图片URL路径信息
-imageDatas = (function getImageURL(imageDatasArr) {
+// 利用自执行函数， 将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+
     // 遍历imageDatasArr数组数据
-    for (var i = 0; i < imageDatasArr.length; i++) {
+    for (var i = 0, j = imageDatasArr.length; i < j; i++) {
         // 获取数组中单个图片数据的信息
-        var singeImageData = imageDatasArr[i];
+        var singleImageData = imageDatasArr[i];
         // 给单个图片数据添加图片路径信息
-        singeImageData.imageURL = require('../images/'
-            + singeImageData.fileName);
+        singleImageData.imageURL = require('../images/' + singleImageData.fileName);
         // 将单个图片数据重新赋值给imageDatasArr[i]
-        imageDatasArr[i] = singeImageData;
+        imageDatasArr[i] = singleImageData;
     }
 
     // 返回imageDatasArr
     return imageDatasArr;
-
 })(imageDatas);
+
+// img标签在react中应该是<img/>，其中的src和alt不可添加引号，否则会报错
+var ImgFigure = React.createClass({
+    render: function () {
+        return (
+            <figure className="img-figure">
+                <img src={this.props.data.imageURL} alt={this.props.data.title}/>
+                <figcaption>
+                    <h2 className="img-title">{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        );
+    }
+});
 
 class AppComponent extends React.Component {
   render() {
+    var controllerUnits = [],
+        imgFigures = [];
+    imageDatas.forEach(function (value) {
+        imgFigures.push(<ImgFigure data={value}/>);
+    });
+
     return (
         <section className="stage">
           <section className="img-sec">
-
+            {imgFigures}
           </section>
           <nav className="controller-nav">
-
+            {controllerUnits}
           </nav>
         </section>
     );
